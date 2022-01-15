45d9e7bae85b7e3dbc7ada13b5f10e21c1b57208
bilgesucakir <bilgesucakir@sabanciuniv.edu>
2022-01-15 20:13:02 +0300

    comments page on profile page implemented

--- lib/routes/myComments_page.dart
+++ lib/routes/myComments_page.dart
@@ -3,6 +3,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cs310_step3/routes/edit_profile.dart';
 import 'package:cs310_step3/routes/pastPurchase.dart';
 import 'package:cs310_step3/routes/seller_profile.dart';
+import 'package:cs310_step3/utils/commentClass.dart';
+import 'package:cs310_step3/utils/prodcutPage.dart';
 import 'package:cs310_step3/utils/productClass.dart';
 import 'package:cs310_step3/utils/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -11,6 +13,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:provider/provider.dart';
 import '/utils/color.dart';
 import '/utils/styles.dart';
@@ -23,19 +26,24 @@ class MyCommentsPage extends StatefulWidget {
   State<MyCommentsPage> createState() => _MyCommentsPageState();
   MyCommentsPage({Key? key, this.myUser}) : super(key: key);
   UserFirebase? myUser;
+  late Product thisProduct;
 
 }
 String theUser = "";
 
 class  _MyCommentsPageState extends State<MyCommentsPage> {
   late UserFirebase? theUser;
-  late List<dynamic> myComments;
+  late List<Comment> myComments = [];
 
-  Future<List<dynamic>> getPosts() async {
-    List<dynamic> myPostsPrev = [];
+  Future<List<Comment>> getPosts() async {
+    List<Comment> myPostsPrev = [];
     theUser = await getUserWithMail(widget.myUser!.email!);
     List<dynamic> myKeys = theUser!.comments!;
-    return myKeys;
+
+    for(Comment myKey in myKeys){
+      myPostsPrev.add(myKey);
+    }
+    return myPostsPrev;
   }
 
   void asyncMethod() async {
@@ -54,8 +62,10 @@ class  _MyCommentsPageState extends State<MyCommentsPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(backgroundColor: AppColors.primary)/*AppBar(
+
+    if(myComments.length > 0){
+      return Scaffold(
+        appBar: AppBar(backgroundColor: AppColors.primary),/*AppBar(
           backgroundColor: AppColors.primary,
           leading: IconButton(
             onPressed: () {
@@ -82,9 +92,58 @@ class  _MyCommentsPageState extends State<MyCommentsPage> {
               icon: Icon(Icons.add_alert),
             ),
           ]
-      ),*/,
-      body: SingleChildScrollView(
-      ),
-    );
+      ),*/
+        body: SingleChildScrollView(
+            child:  ListView.builder(
+                itemCount: myComments.length,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  return Card(
+                    semanticContainer: true,
+                    elevation: 2,
+                    clipBehavior: Clip.antiAliasWithSaveLayer,
+                    child: ListTile(
+                      subtitle: Column(
+                        children: [
+
+                          Text(myComments[index].data!, style: TextStyle(color: Colors.black)),
+                          Text(myComments[index].date!.toString(), style: TextStyle(fontSize: 8, color: Colors.blueGrey)),
+                        ],
+                      ),
+                      trailing: Column(
+                        children: [
+                          RatingBarIndicator(
+                            rating: myComments[index]!.rating!.toDouble(),
+                            itemBuilder: (context, index) => Icon(
+                              Icons.star_rate_outlined,
+                              color: Colors.amber,
+                            ),
+                            itemCount: 5,
+                            itemSize: 5.0,
+                            direction: Axis.horizontal,
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                })
+        ),
+      );
+    }
+    else{
+      return Scaffold(
+        appBar: AppBar(backgroundColor: AppColors.primary),
+
+        body: Container(
+          padding: const EdgeInsets.only(top:50),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("You currently have no comments or ratings to show."),
+            ],
+          ),
+        ),
+      );
+    }
   }
 }
